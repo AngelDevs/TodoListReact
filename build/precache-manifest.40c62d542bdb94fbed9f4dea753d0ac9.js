abeb1f2d4b4ecd80345deb73f1776e541f5e98de
AngelDevs <59152095+AngelDevs@users.noreply.github.com>
2020-01-13 21:07:56 -0400

    Add files via upload

---
+++ build/precache-manifest.40c62d542bdb94fbed9f4dea753d0ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf75649a00f3f89c4f3f5aec795c0f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279f60e88eba88f3ba8c",
+    "url": "/static/css/main.70428148.chunk.css"
+  },
+  {
+    "revision": "8534696c1f5548446846",
+    "url": "/static/js/2.e15845e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e15845e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "279f60e88eba88f3ba8c",
+    "url": "/static/js/main.cf19459d.chunk.js"
+  },
+  {
+    "revision": "06572df58fe3cced68b7",
+    "url": "/static/js/runtime-main.8a8c68f9.js"
+  }
+]);
